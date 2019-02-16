8a54bd158ee65b525588b4b36124d8f6bc1155e1
unknown <16tsmith55@gmail.com>
2019-02-16 10:32:35 -0700

    starting to position elements

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,9 @@ class App extends Component {
   render() {
     return (
       <section>
-        <div className="item item-a" />
-        <div className="item item-b" />
-        <div className="item item-c" />
-        <div className="item item-d" />
-        <div className="item" />
-        <div className="item" />
-        <div className="item" />
-        <div className="item" />
+        <div className="item logo" />
+        <div className="item profile" />
+        <div className="item menu" />
       </section>
     );
   }
