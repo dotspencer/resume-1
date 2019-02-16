5c4bb5e68bd17ee64520ccff5833b69d5496ac0f
unknown <16tsmith55@gmail.com>
2019-02-16 00:37:30 -0700

    quick addition of grid style

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import "../styles/App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <div className="item-a" />
+        <div className="item-b" />
+        <div className="item-c" />
+        <div className="item-d" />
+      </section>
+    );
+  }
+}
+
+export default App;
