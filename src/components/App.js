e19c4a4f685c99db2ec86b64944917c9421b6bd9
unknown <16tsmith55@gmail.com>
2019-02-25 19:18:49 -0700

    added mobile menu

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,38 @@ import "../styles/About.css";
 
 class App extends Component {
   render() {
+    let menuList = document.getElementsByClassName("menuList");
+    let wrapper = document.getElementsByClassName("wrapper");
+    let header = document.getElementsByClassName("header");
+    let menuItems = document.getElementsByClassName("menuItems");
+    let content = document.getElementsByClassName("content");
+
+    function openOverlay() {
+      console.log("opened overlay");
+      menuList[0].classList.add("overlay");
+      header[0].classList.add("darken");
+      wrapper[0].classList.add("cover");
+      content[0].classList.add("noscroll");
+    }
+    function closeOverlay() {
+      console.log("closed overlay");
+      menuList[0].classList.remove("overlay");
+      header[0].classList.remove("darken");
+      wrapper[0].classList.remove("cover");
+      content[0].classList.remove("noscroll");
+    }
+    function toggleOverlay() {
+      if (!menuList[0].classList.contains("overlay")) {
+        openOverlay();
+      } else {
+        window.scrollTo(0, 0);
+        closeOverlay();
+      }
+    }
+
     return (
       <HashRouter>
-        <section>
+        <section className="">
           <section className="header">
             <div className="item logo">
               <div className="full">Trevor J. Smith</div>
@@ -20,30 +49,31 @@ class App extends Component {
             <div className="item menu">
               <div className="mobileBar">
                 <img
-                src="images/menu-icon.svg"
-                alt="profile picture"
-              />
-            </div>
-              <ul>
-                <li className="menuItems">
+                  src="images/menu-icon.svg"
+                  alt="profile picture"
+                  onClick={toggleOverlay}
+                />
+              </div>
+              <ul className="menuList">
+                <li className="menuItems" onClick={toggleOverlay}>
                   <NavLink to="/About" className="menu">
                     About
                   </NavLink>
                 </li>
-                <li className="menuItems">
+                <li className="menuItems" onClick={toggleOverlay}>
                   <NavLink to="/Projects" className="menu">
                     Projects
                   </NavLink>
                 </li>
-                <li className="menuItems">
+                <li className="menuItems" onClick={toggleOverlay}>
                   <NavLink to="/" className="menu">
                     Home
                   </NavLink>
                 </li>
               </ul>
-
             </div>
           </section>
+          <div className="wrapper" onClick={toggleOverlay} />
           <div className="content">
             <Route exact path="/" component={Main} />
             <Route exact path="/About" component={About} />
@@ -54,4 +84,5 @@ class App extends Component {
     );
   }
 }
+
 export default App;
